934f6a627186340079adbaa3e21122db52f48a41
carlcrede <33420878+carlcrede@users.noreply.github.com>
2022-07-26 00:25:57 +0200

    startet individuel side for valgt linje

---
+++ src/pages/ProgrammePage.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { useParams, navigate } from "svelte-navigator";
+    const params = useParams();
+</script>
+
+<style>
+    .wrapper {
+        display: flex;
+    }
+
+    .container {
+
+    }
+</style>
+
+<div class="wrapper">
+    <div class="container">
+        <button on:click="{() => navigate(-1)}">Back</button>
+        <h1>Velkommen til {$params.navn}</h1>
+    </div>
+</div>
