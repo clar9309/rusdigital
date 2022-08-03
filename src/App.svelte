f17669b8c1c81bdc0ed6f4a2a7e730e5441064a5
jimmylosang <47789013+jimmylosang@users.noreply.github.com>
2022-08-03 16:10:51 +0200

    tilføjede fakerjs og svelte-toast til fake aktiviteter

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,35 @@
 		Link,
 		Route,
 	} from "svelte-navigator";
+	import { SvelteToast } from "@zerodevx/svelte-toast";
+	import { faker } from '@faker-js/faker';
 	import FrontPage from './pages/FrontPage.svelte'
 	import About from './pages/About.svelte'
 	import VejlederPage from './pages/VejlederPage.svelte'
 	import ChooseProgrammePage from "./pages/ChooseProgrammePage.svelte";
 	import ProgrammePage from "./pages/ProgrammePage.svelte";
+
+	function randomActivity() {
+		// Random timer i sekunder
+		const randomTimer = Math.floor(Math.random() * 10)
+		// Random name
+		const randomName = faker.name.findName();
+		// Toast
+		toast.push(randomName + " deltager til Dysten om Den Gyldne Støvle!", {
+                theme: {
+                  '--toastBackground': '#F56565',
+                  '--toastBarBackground': '#C53030'
+                }
+            })
+		setTimeout(() => {
+
+		}, randomTimer * 1000)	
+	}
+
+	// Timer så der går 5 sekunder før aktiviteter kan starte
+	setTimeout(() => {
+		randomActivity()
+	}, 500)
 </script>
 <Router>
 	<!-- Navbar -->
